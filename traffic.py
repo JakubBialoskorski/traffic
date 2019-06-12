d6eeb2eeb1488f5528d040ca390d344165455306
Jakub Białoskórski <bialoskorski@icloud.com>
2019-06-12 19:23:49 +0200

    initial commit

---
+++ traffic.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import googlemaps
+import requests
+import sys
+from datetime import datetime
+
+#Enter your Google API Token below
+token = 'PUT_YOUR_TOKEN_HERE'
+start = sys.argv[1]
+end = sys.argv[2]
+now = datetime.now()
+client = googlemaps.Client(key=token)
+
+#Get directions for driving with most pessimistic travel time
+try:
+    directions = client.directions(start,end,alternatives=True,mode="driving",avoid="ferries",departure_time=now,traffic_model="pessimistic")
+
+#Prepare data
+    table = dict()
+    for i in range(len(directions)):
+        table.setdefault(directions[i]["summary"])
+        table[directions[i]["summary"]] = directions[i]["legs"][0]["duration"]["text"]
+    result_list = []
+    for key,value in table.items():
+        result_list.append(' '.join([key,':',value, "\n"]))
+
+#Prepare the output
+    message = ''.join(result_list)
+    print message
+
+#Error handling
+except:
+    error = 'Error during processing'
+    print 'Error'
